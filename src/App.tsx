d3442755cc0155444f36815f9289c99a35a74f37
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2025-03-10 02:00:52 -0300

    Created Project and NavBar

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+import NavBar from './components/NavBar'
+
+
+export default function App() {
+ 
+
+  return (
+    <>
+    
+    <NavBar/>
+    <main>
+
+    </main>
+   
+    
+    </>
+  )
+}
+
+
---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,16 @@
+
+import './styles.css';
+
+export default function NavBar(){
+    return(
+        <header>
+            <nav className='ct-container'>
+                <h1> DSCarrosTop </h1>
+            </nav>
+        </header>
+      
+
+    );
+
+
+}
